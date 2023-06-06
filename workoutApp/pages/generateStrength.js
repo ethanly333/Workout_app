5483f0f32cb62f2dd4f95f54fc36a7f5cc660808
ethanly <ethanly8617@gmail.com>
2023-06-06 14:39:38 -0500

    strength workout generator working

--- workoutApp/pages/generateStrength.js
+++ workoutApp/pages/generateStrength.js
@@ -6,22 +6,70 @@ import { strengthWorkouts } from '../data/workouts';
 
 export default function GenerateStrengthSCreen()
 {
-    var workout = [] ;
-
     const [genModalState, setGenModalState] = useState(false) ;
+    const [workoutState, setWorkoutState] = useState([]) ;
 
     const genBtnHandler = () =>
     {
-        
+        var workoutIndx = [] ;
+        var workout = [] ;
+
+        var count = 0 ;
+        while(count < 5)
+        {
+            var randNum = Math.floor(Math.random()*strengthWorkouts.length) ;
+            if(!workoutIndx.includes(randNum))
+            {
+                workoutIndx.push(randNum) ;
+                count++ ;
+            }//end if
+        }//end for i
+
+        for(var j=0; j<workoutIndx.length; j++)
+        {
+            workout.push(strengthWorkouts[workoutIndx[j]]) ;
+        }//end for j
+
+        setWorkoutState(workout) ;
+        setGenModalState(true) ;
     }; //end genBtnHandler
 
     return (
         <SafeAreaView style={STRENGTHSTYLE.mainContainer}>
             <TouchableOpacity
                 style = {STRENGTHSTYLE.generateBtn}
+                onPress={() => {genBtnHandler()}}
             >
-                <Text style={{color: 'black', fontSize: 20}}>Generate</Text>
+                <Text style={{
+                    color: 'black', 
+                    fontSize: 23, 
+                    fontWeight: 600
+                    }}>
+                        Generate
+                </Text>
             </TouchableOpacity>
+            <Modal
+                isVisible = {genModalState} 
+                onBackdropPress={() => {setGenModalState(false); }}
+            >
+                <SafeAreaView style={STRENGTHSTYLE.workoutModalContainer}>
+                    <ScrollView>
+                    {workoutState.map((exercise) =>     
+                        <SafeAreaView>
+                            <TouchableOpacity style={STRENGTHSTYLE.workoutModalBtns}>
+                                <Text style={{
+                                    fontSize: 23,
+                                    color: 'white',
+                                    margin: 10,
+                                    }}>
+                                        {exercise.title}
+                                </Text>
+                            </TouchableOpacity>
+                        </SafeAreaView>
+                    )}
+                    </ScrollView>
+                </SafeAreaView>
+            </Modal>
         </SafeAreaView>
     )
 }
